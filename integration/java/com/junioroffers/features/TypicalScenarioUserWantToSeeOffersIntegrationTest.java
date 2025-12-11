2e94132282e8b65c804ba93633feac5fc87bf00d
@thiankiann <thiankiann@gmail.com>
2025-12-11 11:40:59 +0000

    Integration Test for GET /offers/9999 and system returned NOT_FOUND(404) (not existing RestController yet)

--- integration/java/com/junioroffers/features/TypicalScenarioUserWantToSeeOffersIntegrationTest.java
+++ integration/java/com/junioroffers/features/TypicalScenarioUserWantToSeeOffersIntegrationTest.java
@@ -22,6 +22,7 @@ import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 //import static com.junioroffers.features.SampleJobOfferResponse.bodyWithZeroOffersJson;
@@ -77,6 +78,19 @@ public class TypicalScenarioUserWantToSeeOffersIntegrationTest extends BaseInteg
 //    step 9: scheduler ran 2nd time and made GET to external server and system added 2 new offers with ids: 1000 and 2000 to database
 //    step 10: user made GET /offers with header “Authorization: Bearer AAAA.BBBB.CCC” and system returned OK(200) with 2 offers with ids: 1000 and 2000
 //    step 11: user made GET /offers/9999 and system returned NOT_FOUND(404) with message “Offer with id 9999 not found”
+        // given
+        // when
+        ResultActions performGetResultsWithNotExistingId = mockMvc.perform(get("/offers/notExistingId"));
+        // then
+        performGetResultsWithNotExistingId.andExpect(status().isNotFound())
+                .andExpect(content().json("""
+                        {
+                        "message": “Offer with id 9999 not found”,
+                        "status": "NOT_FOUND"
+                        }
+                        """.trim()
+                ));
+
 //    step 12: user made GET /offers/1000 and system returned OK(200) with offer
 //    step 13: there are 2 new offers in external HTTP server
 //    step 14: scheduler ran 3rd time and made GET to external server and system added 2 new offers with ids: 3000 and 4000 to database
