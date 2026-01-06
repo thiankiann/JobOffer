ae02963b8fd0d16cb27ea8bbd0ac6fcc64d47594
@thiankiann <thiankiann@gmail.com>
2026-01-06 12:57:02 +0000

    prototype of Post request + basic Integration test (of returning status 201(created)) is working

--- src/main/java/com/junioroffers/domain/offer/dto/OfferRequestDto.java
+++ src/main/java/com/junioroffers/domain/offer/dto/OfferRequestDto.java
@@ -2,11 +2,25 @@ package com.junioroffers.domain.offer.dto;
 
 import lombok.Builder;
 
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+
 @Builder
 public record OfferRequestDto(
+        @NotNull(message = "{inputNumbers.not.null}")
+        @NotEmpty(message = "{inputNumbers.not.empty}")
         String companyName,
+
+        @NotNull(message = "{inputNumbers.not.null}")
+        @NotEmpty(message = "{inputNumbers.not.empty}")
         String position,
+
+        @NotNull(message = "{inputNumbers.not.null}")
+        @NotEmpty(message = "{inputNumbers.not.empty}")
         String salary,
+
+        @NotNull(message = "{inputNumbers.not.null}")
+        @NotEmpty(message = "{inputNumbers.not.empty}")
         String offerUrl
 ) {
 }
